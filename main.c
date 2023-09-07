3b063439fa0d8f7b7606bca9c61fa0a6d1bb73d9
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-09-07 13:25:57 -0500

    Added LED and push button functionality

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #include "strings.h"
 #include "commands.h"
 #include "pinMappings.h"
+#include "nvic.h"
 
 #define IS_COMMAND(string, count)       if(isCommand(&shellData, string, count))
 #define RESET                           (NVIC_APINT_R = (NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ))
@@ -21,24 +22,46 @@
 /**
  *      @brief Function to initialize all necessary hardware on the device
  **/
-void init_TM4C_hardware(void)
+void initTm4c(void)
 {
     initSystemClockTo40Mhz(); 		                // Initialize system clock
 
+    enablePort(PORTA);                              // Initialize clocks on PORT A
+    enablePort(PORTB);                              // Initialize clocks on PORT B
+    enablePort(PORTC);                              // Initialize clocks on PORT B
     enablePort(PORTF);                              // Initialize clocks on PORT F
-    selectPinPushPullOutput(LED_R);                 // Initialise red LED gpio as output
-    selectPinPushPullOutput(LED_G);                 // Initialise green LED gpio as output
-    selectPinPushPullOutput(LED_B);                 // Initialise blue LED gpio as output
+
+    selectPinDigitalInput(PUB_E1);                  // Initialise pin as input
+    selectPinDigitalInput(PUB_E2);                  // Initialise pin as input
+    selectPinDigitalInput(PUB_E3);                  // Initialise pin as input
+    selectPinDigitalInput(PUB_E4);                  // Initialise pin as input
+    selectPinDigitalInput(PUB_E5);                  // Initialise pin as input
+    selectPinDigitalInput(PUB_E6);                  // Initialise pin as input
+
+    enablePinPullup(PUB_E1);                        // Enable internal pull-up
+    enablePinPullup(PUB_E2);                        // Enable internal pull-up
+    enablePinPullup(PUB_E3);                        // Enable internal pull-up
+    enablePinPullup(PUB_E4);                        // Enable internal pull-up
+    enablePinPullup(PUB_E5);                        // Enable internal pull-up
+    enablePinPullup(PUB_E6);                        // Enable internal pull-up
+
+    selectPinPushPullOutput(LED_R);                 // Initialise pin as output
+    selectPinPushPullOutput(LED_G);                 // Initialise pin as output
+    selectPinPushPullOutput(LED_B);                 // Initialise pin as output
+
+    selectPinPushPullOutput(LED_EY);                // Initialise pin as output
+    selectPinPushPullOutput(LED_EG);                // Initialise pin as output
+    selectPinPushPullOutput(LED_ER);                // Initialise pin as output
+    selectPinPushPullOutput(LED_EO);                // Initialise pin as output
 
     setPinValue(LED_R, 1);
     setPinValue(LED_G, 1);
     setPinValue(LED_B, 1);
 
-    waitMicrosecond(20000);
-
-    setPinValue(LED_R, 0);
-    setPinValue(LED_G, 0);
-    setPinValue(LED_B, 0);
+    setPinValue(LED_EY, 1);
+    setPinValue(LED_EG, 1);
+    setPinValue(LED_ER, 1);
+    setPinValue(LED_EO, 1);
 
     initUart0();                                    // Initialise UART0
     setUart0BaudRate(115200, 40e6);                 // Set UART baud rate and clock
@@ -49,11 +72,18 @@ void init_TM4C_hardware(void)
  **/
 void main(void)
 {
-    init_TM4C_hardware();
+    initTm4c();
 
     shellData_t shellData;
     while (1)
     {
+        if(!getPinValue(PUB_E1)) setPinValue(LED_R, 0);
+        if(!getPinValue(PUB_E2)) setPinValue(LED_G, 0);
+        if(!getPinValue(PUB_E3)) setPinValue(LED_EO, 0);
+        if(!getPinValue(PUB_E4)) setPinValue(LED_EY, 0);
+        if(!getPinValue(PUB_E5)) setPinValue(LED_EG, 0);
+        if(!getPinValue(PUB_E6)) setPinValue(LED_ER, 0);
+#if 0
         getInputString(&shellData);         // Read user input
         parseInputString(&shellData);       // Parse user input
 
@@ -119,5 +149,6 @@ void main(void)
         }
 
         print((void *)"", "Invalid input", CHAR);
+#endif
     }
 }
--- pinMappings.h
+++ pinMappings.h
@@ -1,8 +1,20 @@
 #ifndef PIN_MAPPINGS_H
 #define PIN_MAPPINGS_H
 
-#define LED_R   PORTF,1             // Red LED pin mapping
-#define LED_B   PORTF,2             // Blue LED pin mapping
-#define LED_G   PORTF,3             // Green LED pin mapping
+#define LED_R   PORTF,1     // Internal LED pin
+#define LED_B   PORTF,2     // Internal LED pin
+#define LED_G   PORTF,3     // Internal LED pin
+
+#define LED_EY  PORTC,4     // External LED pin
+#define LED_EG  PORTC,5     // External LED pin
+#define LED_ER  PORTC,6     // External LED pin
+#define LED_EO  PORTC,7     // External LED pin
+
+#define PUB_E1  PORTA,2     // External push button
+#define PUB_E2  PORTA,3     // External push button
+#define PUB_E3  PORTA,4     // External push button
+#define PUB_E4  PORTB,6     // External push button
+#define PUB_E5  PORTB,7     // External push button
+#define PUB_E6  PORTB,2     // External push button
 
 #endif
