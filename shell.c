5149cdcd5fb748a1373c023088c7945476d6dc43
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-09-20 19:21:48 -0500

    Moved shell interface to dedicated file

---
+++ shell.c
@@ -0,0 +1,61 @@
+#include "strings.h"
+#include "commands.h"
+#include "tm4c123gh6pm.h"
+
+#define IS_COMMAND(string, count)       if(isCommand(&shellData, string, count))
+#define RESET                           (NVIC_APINT_R = (NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ))
+#define ASSERT(value)                   if(value >= 0)
+
+
+void shell(void)
+{
+    shellData_t shellData;
+
+    if (kbhitUart0())
+    {
+        getInputString(&shellData);         // Read user input
+        parseInputString(&shellData);       // Parse user input
+
+        IS_COMMAND("ps", 1)     ps();                           // Invoke function
+        IS_COMMAND("reboot", 1) RESET;                          // Reset System
+        IS_COMMAND("ipcs", 1)   ipcs();                         // Invoke function
+        IS_COMMAND("kill", 2)
+        {
+            uint32_t pid = (uint32_t)getFieldInteger(&shellData, 1);
+            kill(pid);                      // Invoke function
+        }
+
+        IS_COMMAND("pkill", 2)              // Invoke function
+        {
+            char *procName = getFieldString(&shellData, 1);
+            toLower(procName);
+            Pkill(procName);
+        }
+
+        IS_COMMAND("preempt", 2)
+        {
+            char *preemptionState = getFieldString(&shellData, 1);
+            preempt(toBool(preemptionState));
+        }
+
+        IS_COMMAND("sched", 2)
+        {
+            char *scheduleState = getFieldString(&shellData, 1);
+            sched(toBool(scheduleState));
+        }
+
+        IS_COMMAND("pidof", 2)
+        {
+            char *procName = getFieldString(&shellData, 1);
+            pidof(procName);
+        }
+
+        IS_COMMAND("run", 2)
+        {
+            char *procName = getFieldString(&shellData, 1);
+            run(procName);
+        }
+
+        print((void *)"", "Invalid input", CHAR);
+    }
+}
