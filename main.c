b6c5bcbbd12f87d30a490e6b948cd463b082bd06
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-09-24 16:10:55 -0500

    Moved exception testing to dedicated files

--- main.c
+++ main.c
@@ -6,47 +6,9 @@
 
 #include "tm4c123gh6pm.h"
 #include "gpio.h"
-#include "wait.h"
 #include "shell.h"
 #include "pinMappings.h"
-#include "systemInterrups.h"
-
-/**
-*      @brief Function to cause a bus fault
-**/
-void busFaultTrigger(void)
-{
-    uint32_t *tester = (uint32_t *)0xFFFFFFFF;      // Access out-of-bounds pointer
-    *tester = 10;                                   // Trigger a bus fault
-}
-
-/**
- *      @brief Function to cause a usage fault
- **/
-void usageFaultTrigger(void)
-{
-    uint8_t div = 0;
-    uint8_t res = 10 / div;                         // Trigger a divide by zero fault
-}
-
-/**
-*      @brief Function to cause a usage fault
-**/
-void pendSVTrigger(void)
-{
-    enablePendSV();                                 // Set PendSV
-}
-
-/**
-*      @brief Functions to cause a memory management fault
-**/
-typedef void (*FunctionPointer)(void);              // Define a function pointer type for a function that takes no arguments and returns void
-
-void customFunction()                               // Define a function at the desired address (0x00040000)
-{
-    uint8_t a = 5, b = 10;
-    a = a + b;
-}
+#include "exceptionTests.h"
 
 /**
  *      @brief Main, driver function
