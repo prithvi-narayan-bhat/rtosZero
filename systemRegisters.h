3b4bf0c0abd25b93a41b261109df9144e4066b86
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-09-26 12:55:43 -0500

    Cleanup

--- systemRegisters.h
+++ systemRegisters.h
@@ -1,9 +1,13 @@
 #ifndef SYSTEM_REGISTERS_H
 #define SYSTEM_REGISTERS_H
 
+#include <inttypes.h>
+
 extern void switchToPSP(uint32_t value);            // Function to switch operation control to PSP from MSP
 extern uint32_t getPSP(void);                       // Function to read and return the value of the PSP register
 extern uint32_t getMSP(void);                       // Function to read and return the value of the MSP register
 extern uint32_t getFaultFlags(uint32_t *regAddr);   // Function to read the fault flags
+extern uint32_t disablePrivilegedMode(void);        // Function to disable Privileged execution mode
+extern uint32_t enablePrivilegedMode(void);         // Function to enable Privileged execution mode
 
 #endif
