553858b4e594bdaba44007c2e1b52b96b520b63c
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-10-01 16:57:29 -0500

    Reduced size of metaData structure

--- memory.c
+++ memory.c
@@ -16,19 +16,21 @@
 #define SUBREGIONS_512(size)    (size / BLOCK_SIZE_1)   // Determine the number of 512B sub regions required for the allotment
 #define SUBREGIONS_1024(size)   (size / BLOCK_SIZE_2)   // Determine the number of 1024B sub regions required for the allotment
 
+#define RETURN_VALID    if (retVal != NULL) return (void *)retVal
+#define RETURN_INVALID  return (void *)NULL
+
 /**
 *      @brief Structure to hold the metadata of the Heap
 **/
 typedef struct
 {
-    uint32_t *address;          // Base of the allocation
-    uint16_t size;              // Size of the allocation starting at the base
-    uint8_t subRegions;         // Number of subregions allocated here
+    void *address;                                              // Base of the allocation
+    uint8_t subRegions;                                         // Number of subregions allocated here
 } heapMetadata_t;
 
 // Global variables
 uint8_t heapTop_g = 0, allotment_g[TOTAL_REGIONS] = {0, };      // A ledger to keep track allocated subregions, initialised to 0
-heapMetadata_t heapMetadata_g[TOTAL_REGIONS] = {{0, 0, 0}, };   // Initialise allotment metadata
+heapMetadata_t heapMetadata_g[TOTAL_REGIONS] = {{0, 0}, };      // Initialise allotment metadata
 
 /**
  *      @brief Get the Allocation object
@@ -43,12 +45,12 @@ heapMetadata_t heapMetadata_g[TOTAL_REGIONS] = {{0, 0, 0}, };   // Initialise al
  *      @param blockSize size of each subregion in the region
  *      @return void* an address to the base of allocated heap space
  **/
-void *getAllocation(uint32_t size, uint8_t subRegions, uint8_t startRange, uint8_t endRange, uint32_t baseAddr, int16_t offset, uint16_t blockSize)
+void *getAllocation(uint8_t subRegions, uint8_t startRange, uint8_t endRange, uint32_t baseAddr, int16_t offset, uint16_t blockSize)
 {
     uint8_t fragmented = 0, emptySubRegions = 0, index;
 
     // Check for space in the first 8K region
-    for (index = startRange; index <= endRange; index++)      // +-1 to leave space for 1.5K blocks
+    for (index = startRange; index <= endRange; index++)                            // +-1 to leave space for 1.5K blocks
     {
         if (!(allotment_g[index]))                                                  // Check if space is unallocated
         {
@@ -69,9 +71,8 @@ void *getAllocation(uint32_t size, uint8_t subRegions, uint8_t startRange, uint8
                     emptySubRegions--;
                 }
 
-                heapMetadata_g[heapTop_g].size = size;                              // Record size and allocated address
-                heapMetadata_g[heapTop_g].subRegions = subRegions;
-                heapMetadata_g[heapTop_g].address = (uint32_t *)(baseAddr + ((index + (offset)) * blockSize));
+                heapMetadata_g[heapTop_g].subRegions = subRegions;                  // Record size and allocated address
+                heapMetadata_g[heapTop_g].address = (void *)(baseAddr + ((index + (offset)) * blockSize));
 
                 return (void *)heapMetadata_g[heapTop_g++].address;                 // Return the address
             }
@@ -91,8 +92,7 @@ void *getAllocation(uint32_t size, uint8_t subRegions, uint8_t startRange, uint8
  **/
 void *malloc_from_heap(int32_t size)
 {
-    uint8_t subRegions, block;
-    int32_t *retVal;
+    void *retVal;
 
     // Standardize size
     if      (size <= BLOCK_SIZE_1)  size = BLOCK_SIZE_1;
@@ -102,40 +102,40 @@ void *malloc_from_heap(int32_t size)
     // If request is for one of three 1.5K intersections
     if (size == BLOCK_SIZE_3)
     {
-        retVal = getAllocation(size, (2), (BLOCK_4K1_END), (BLOCK_8K1_START), REGION_4K1_BASE_ADDR, 1, BLOCK_SIZE_1);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((2), (BLOCK_4K1_END), (BLOCK_8K1_START), REGION_4K1_BASE_ADDR, 1, BLOCK_SIZE_1);
+        RETURN_VALID;
 
-        retVal = getAllocation(size, (2), (BLOCK_8K1_END), (BLOCK_4K2_START), REGION_8K1_BASE_ADDR, -7, BLOCK_SIZE_2);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((2), (BLOCK_8K1_END), (BLOCK_4K2_START), REGION_8K1_BASE_ADDR, -7, BLOCK_SIZE_2);
+        RETURN_VALID;
 
-        retVal = getAllocation(size, (2), (BLOCK_4K3_END), (BLOCK_8K2_START), REGION_4K3_BASE_ADDR, -23, BLOCK_SIZE_1);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((2), (BLOCK_4K3_END), (BLOCK_8K2_START), REGION_4K3_BASE_ADDR, -23, BLOCK_SIZE_1);
+        RETURN_VALID;
     }
 
     // Make an allotment from one of the three 4K blocks
-    else if (SUBREGIONS_512(size) < SUBREGIONS_1024(size) || !SUBREGIONS_1024(size))
+    if (SUBREGIONS_512(size) < SUBREGIONS_1024(size) || !SUBREGIONS_1024(size))
     {
-        retVal = getAllocation(size, (SUBREGIONS_512(size)), (BLOCK_4K1_START), (BLOCK_4K1_END - 1), REGION_4K1_BASE_ADDR, 0, BLOCK_SIZE_1);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((SUBREGIONS_512(size)), (BLOCK_4K1_START), (BLOCK_4K1_END - 1), REGION_4K1_BASE_ADDR, 0, BLOCK_SIZE_1);
+        RETURN_VALID;
 
-        retVal = getAllocation(size, (SUBREGIONS_512(size)), (BLOCK_4K2_START + 1), BLOCK_4K2_END, REGION_4K2_BASE_ADDR, -15, BLOCK_SIZE_1);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((SUBREGIONS_512(size)), (BLOCK_4K2_START + 1), BLOCK_4K2_END, REGION_4K2_BASE_ADDR, -15, BLOCK_SIZE_1);
+        RETURN_VALID;
 
-        retVal = getAllocation(size, (SUBREGIONS_512(size)), (BLOCK_4K3_START), (BLOCK_4K3_END - 1), REGION_4K3_BASE_ADDR, -22, BLOCK_SIZE_1);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((SUBREGIONS_512(size)), (BLOCK_4K3_START), (BLOCK_4K3_END - 1), REGION_4K3_BASE_ADDR, -22, BLOCK_SIZE_1);
+        RETURN_VALID;
     }
 
     // Make an allotment from one of the two 8K blocks
     else
     {
-        retVal = getAllocation(size, (SUBREGIONS_1024(size)), (BLOCK_8K1_START + 1), (BLOCK_8K1_END - 1), REGION_8K1_BASE_ADDR, -7, BLOCK_SIZE_2);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((SUBREGIONS_1024(size)), (BLOCK_8K1_START + 1), (BLOCK_8K1_END - 1), REGION_8K1_BASE_ADDR, -7, BLOCK_SIZE_2);
+        RETURN_VALID;
 
-        retVal = getAllocation(size, (SUBREGIONS_1024(size)), (BLOCK_8K2_START + 1), (BLOCK_8K2_END), REGION_8K2_BASE_ADDR, -31, BLOCK_SIZE_2);
-        if (retVal != NULL) return (void *)retVal;
+        retVal = getAllocation((SUBREGIONS_1024(size)), (BLOCK_8K2_START + 1), (BLOCK_8K2_END), REGION_8K2_BASE_ADDR, -31, BLOCK_SIZE_2);
+        RETURN_VALID;
     }
 
-    return (void *)NULL;
+    RETURN_INVALID;
 }
 
 void free(void *baseAdd)
@@ -144,9 +144,8 @@ void free(void *baseAdd)
 
     for (index = 0; index < TOTAL_REGIONS; index++)
     {
-        if (heapMetadata_g[index].address == (uint32_t *)baseAdd)
+        if (heapMetadata_g[index].address == baseAdd)
         {
-            heapMetadata_g[index].size = 0;
             subRegions = heapMetadata_g[index].subRegions;
             heapMetadata_g[index].subRegions = 0;
 
