2c7f7cda1453f4a887f89fb7baf642e3346d4f50
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-11-23 14:11:37 -0600

    Added support for Pkill, preempt, kill, sched, pidof, run, ipcs, priority

--- commands.h
+++ commands.h
@@ -13,8 +13,9 @@ void ipcs(void);            // Function to display the inter-process (thread) co
 void kill(uint32_t pid);    // Function to kill the process (thread) with matching PID
 void Pkill(char *procName); // Function to kill process (thread) with matching name
 void preempt(bool state);   // Function to toggle preemption state
-void sched(bool state);     // Function to determine scheduling order
+void priority(bool state);  // Function to determine scheduling order
 void pidof(char *procName); // Function to display the PID of given process
 void run(char *procName);   // Function to run selected program in the background
+void reboot(void);         // Function to reset the system
 
 #endif
